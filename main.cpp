c17651e4098d3322007931e1483b6d8d3e16f135
cbpalumbi <christabellapalumbi@gmail.com>
2026-03-15 19:30:45 -0400

    Add default asset directory

--- main.cpp
+++ main.cpp
@@ -1,27 +1,29 @@
 ﻿#include <iostream>
 #include <filesystem>
 namespace fs = std::filesystem;
+const std::string ASSETS_DIR = "C:/Users/Bella/CLionProjects/AssetRegistry/assets";
 
-bool isValidPath(const std::string& pathStr) {
-    fs::path p(pathStr);
-
-    if (fs::exists(p)) {
-        return true;
-    }
-
-    return false;
+bool isValidPath(const fs::path& p) {
+    return fs::exists(p) && !fs::is_directory(p);
 }
 
 int main() {
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    std::cout << "Assets dir: " << ASSETS_DIR << "\n";
 
     while (true) {
+        std::cout << "Which asset would you like to load?: ";
         std::string input;
         std::cin >> input;
-        std:: cout << input << "\n";
-        std::cout << isValidPath(input) << "\n";
+
+        fs::path p(ASSETS_DIR);
+        p += "/" + input;
+
+        if (isValidPath(p)) {
+            std::cout << "File '" << input << "' EXISTS at path " << p << "\n";
+        } else {
+            std::cout << "File '" << input << "' does not exist at path " << p << "\n";
+        }
     }
 
     return 0;
