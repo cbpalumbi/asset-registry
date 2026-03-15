7122fe69f17c70323644c2fee886faf356da3d8d
cbpalumbi <christabellapalumbi@gmail.com>
2026-03-15 18:35:55 -0400

    Set up

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <filesystem>
+
+bool isValidPath(std::string path) {
+
+    if (path == "hello") {
+        return true;
+    }
+
+    return false;
+}
+
+int main() {
+
+    const auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    while (true) {
+        std::string input;
+        std::cin >> input;
+        std:: cout << input << "\n";
+        std::cout << isValidPath(input) << "\n";
+    }
+
+    return 0;
+}
